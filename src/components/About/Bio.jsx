3662704ff0bea3adb65bbcb930cca3e68942b968
carolina <carolinacapetilloc@gmail.com>
2020-10-07 13:43:48 -0700

    changes bio info

--- src/components/About/Bio.jsx
+++ src/components/About/Bio.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Bio = (props) => {
     return (
     <div>
-        <p>Hey there! My name is Carolina Capetillo and I'm a full-stack developer based in Santa Barbara, CA.</p>
+        <p>Hey there! My name is Carolina Capetillo and 
+            I'm a full-stack developer based in Santa Barbara, CA. 
+            I'm fluent in English, Spanish, Italian, JavaScript, and Python!</p>
     </div>
     )
 }
