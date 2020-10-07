d4946faab8a54420b17134e446116d0a19eb6bac
carolina <carolinacapetilloc@gmail.com>
2020-10-07 13:47:02 -0700

    adds intro

--- src/components/About/Interests.jsx
+++ src/components/About/Interests.jsx
@@ -5,7 +5,7 @@ const Interests = (props) => {
     <div>
         <p>I enjoy creating apps, websites, and extensions for myself and clients.
               My goal is to always build products that are 
-              as appealing as they are functional with readable code.</p>
+              appealing and functional with readable code.</p>
     </div>
     )
 }
---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Intro.css';
+
+const Intro = (props) => {
+    return (
+    <div className="intro">
+        <h1>Hello! I'm Carolina Capetillo.</h1>
+        <h1>SOME CATCHPHRASE I GUESS</h1>
+        <p>I'm a full-stack developer based in Santa Barbara, California and I 
+            enjoy creating and designing beautfiul games, applications, websites, and extensions.
+        </p>
+    </div>
+    )
+}
