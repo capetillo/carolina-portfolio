051c38e2cd8863f65b72c0a575ba522a8e4ff7b5
carolina <carolinacapetilloc@gmail.com>
2020-10-08 15:39:31 -0700

    styles intro (padding)

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import './Intro.css';
+import {Row, Col} from 'react-bootstrap';
 
 const Intro = (props) => {
     return (
     <div className="intro">
-        <h1>Hello! I'm Carolina Capetillo.</h1>
-        <h1>SOME CATCHPHRASE I GUESS</h1>
-        <p>I'm a full-stack developer based in Santa Barbara, California. I 
-            create and design beautfiul games, applications, websites, and extensions.
-        </p>
+        <Row className="py-5 px-4">
+            <Col sm={12}>
+                <h1>Hello! I'm Carolina Capetillo.</h1>
+                <h1>SOME CATCHPHRASE I GUESS</h1>
+            </Col>
+            <Col sm={12}>
+                <p>I'm a full-stack developer based in Santa Barbara, California.
+    
+                  
+                    I 
+                    create and design beautfiul games, applications, websites, and extensions.
+                </p>
+            </Col>
+        </Row>
     </div>
     )
 }
