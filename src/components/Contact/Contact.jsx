0e00b1365c08c6948e4831cf337925973b9bf3d8
carolina <carolinacapetilloc@gmail.com>
2020-10-08 18:20:48 -0700

    adds form to send email

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,14 +1,49 @@
 import React from 'react';
-import './Contact.css'
+import './Contact.css';
+import emailjs from 'emailjs-com';
+import {Row, Col} from 'react-bootstrap';
 
-const Contact = (props) => {
-    return (
-        <div className="contact" id="contact">
-            <h1 className="contactme">contact me </h1>
-            <p>phone number: 61960090790</p> 
-            <p>email: carolinacapetilloc@gmail.com</p>
-        </div>
-    )
+export default function Contact() {
+
+    function sendEmail(e) {
+        e.preventDefault();
+    
+        emailjs.sendForm('gmail', 'template_73mzcao', e.target, 'user_JzyA9mMxX3G7uexfqc5uS')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+          e.target.reset()
+    }
+    
+    
+        return (
+            <div className="contact" id="contact">
+                <h1 className="contactme">contact me </h1>
+                <form onSubmit={sendEmail}>
+                    <Row className="pt-5 mx-auto">
+                        <Col className="form-group mx-auto">
+                            <input type="text" className="form-control" placeholder="name" name="name"/>
+                        </Col>
+                        <Col className="form-group mx-auto">
+                            <input type="email" className="form-control" placeholder="email address" name="email"/>
+                        </Col>
+                        <Col className="form-group mx-auto">
+                            <input type="text" className="form-control" placeholder="subject" name="subject"/>
+                        </Col>
+                        <Col className="form-group pt-2 mx-auto">
+                            <textarea className="form-control" cols="30" rows="8" placeholder="your message" name="message"></textarea>
+                        </Col>
+                        <Col className="form-group pt-3 mx-auto">
+                            <input type="submit" className="btn btn-info" value="send message"/>
+                        </Col>
+                    </Row>
+                </form>
+            </div>
+        )
+
 }
 
-export default Contact
+
+
