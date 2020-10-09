83cab47f24f9cfaad94d60697c128a627354e3e9
carolina <carolinacapetilloc@gmail.com>
2020-10-09 12:17:35 -0700

    gets rid of glitch-y expand form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Contact.css';
 import emailjs from 'emailjs-com';
-import {Row, Col, Button, Fade} from 'react-bootstrap';
+import {Row, Col, Button, Collapse} from 'react-bootstrap';
 
 export default function Contact() {
 
@@ -16,37 +16,42 @@ export default function Contact() {
           });
           e.target.reset()
     }
-
-
-      
-    
         return (
             <div className="contact" id="contact">
-                <h1 className="contactme">contact me </h1>
-                <Button className="collapsed" type="button" data-toggle="collapse" data-target="#form-collapse-text" aria-controls="form-collapse-text" aria-expanded="false" aria-label="Toggle button">
-                contact me
-                 </Button>
-                <Fade in="true">
-                <form onSubmit={sendEmail} >
-                    <Row className="pt-5 mx-auto collapse" id="form-collapse-text">
-                        <Col className="form-group mx-auto">
-                            <input type="text" className="form-control" placeholder="name" name="name"/>
-                        </Col>
-                        <Col className="form-group mx-auto">
-                            <input type="email" className="form-control" placeholder="email address" name="email"/>
-                        </Col>
-                        <Col className="form-group mx-auto">
-                            <input type="text" className="form-control" placeholder="subject" name="subject"/>
-                        </Col>
-                        <Col className="form-group pt-2 mx-auto">
-                            <textarea className="form-control" cols="30" rows="8" placeholder="your message" name="message"></textarea>
-                        </Col>
-                        <Col className="form-group pt-3 mx-auto">
-                            <input type="submit" className="btn btn-info" value="send message"/>
-                        </Col>
-                    </Row>
-                </form>
-                </Fade>
+                <h1 className="contactme">contact me</h1>
+                <p>I'm currently looking for work! Feel free to reach out if you have any questions, comments, suggestions, or just want to say hello!</p>
+                <div className="email-form">
+                    <div className="email-button">
+                        <Button className="collapsed justify-self-center" type="button" data-toggle="collapse" data-target="#form-collapse-text" aria-controls="form-collapse-text" aria-expanded="false" aria-label="Toggle button">
+                        get in touch
+                        </Button>
+                    </div>
+                    <Collapse in="true">
+                   
+                    <form onSubmit={sendEmail} >
+                        
+                            <Row className="py-5 mx-auto collapse" id="form-collapse-text">
+                                <Col className="form-group mx-auto" sm={12} >
+                                    <input type="text" className="form-control" placeholder="name" name="name"/>
+                                </Col>
+                                <Col className="form-group mx-auto" sm={12}>
+                                    <input type="email" className="form-control" placeholder="email address" name="email"/>
+                                </Col>
+                                <Col className="form-group mx-auto" sm={12}>
+                                    <input type="text" className="form-control" placeholder="subject" name="subject"/>
+                                </Col>
+                                <Col className="form-group pt-2 mx-auto" sm={12}>
+                                    <textarea className="form-control" cols="30" rows="8" placeholder="your message" name="message"></textarea>
+                                </Col>
+                                <Col className="form-group pt-3 mx-auto" sm={12}>
+                                    <input type="submit" className="btn btn-info" value="send message"/>
+                                </Col>
+                            </Row>
+                        
+                    </form>
+                 
+                    </Collapse>
+                </div>
             </div>
         )
 
